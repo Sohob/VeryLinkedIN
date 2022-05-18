ae5f1206ff6a9b195202f507e6d589f3a4497cfd
Mohamed Sami <ma-771@hotmail.com>
2022-05-18 23:40:59 +0200

    Update Comment class

--- verylinkedin/mypost/src/main/java/com/verylinkedin/mypost/models/Comment.java
+++ verylinkedin/mypost/src/main/java/com/verylinkedin/mypost/models/Comment.java
@@ -3,8 +3,27 @@ package com.verylinkedin.mypost.models;
 import org.springframework.data.annotation.Id;
 
 public class Comment {
-    @Id
     private String id;
-    @Id
     private String userId;
+
+    public Comment(String id, String userId) {
+        this.id = id;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
