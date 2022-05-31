b6c680b2b0529c06d31a0df5aed4803c29a9112b
Mohamed Sami <ma-771@hotmail.com>
2022-05-31 22:33:41 +0200

    Update PostTest

--- verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/models/PostTest.java
+++ verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/models/PostTest.java
@@ -1,13 +1,25 @@
 package com.verylinkedin.mypost.models;
 
+import com.verylinkedin.mypost.PostRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PostTest {
 
+class PostTest {
+    @Autowired
+    private PostRepository postRepository;
     @Test
-    void getUserId() {
+    void createPostTest() {
+        Post post = Post.builder()
+                .userId("123456789")
+                .content("Test")
+                .build();
+        post.setPublic(true);
+        postRepository.save(post);
+
+        assertEquals(postRepository.findById("123456789"), true);
     }
 
     @Test
