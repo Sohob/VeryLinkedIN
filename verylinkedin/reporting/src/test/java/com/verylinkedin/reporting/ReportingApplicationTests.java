8f72c632d710e7ed2fe1dc6950adeeef39568191
Ibrahim Dewedar <41593808+Hemose@users.noreply.github.com>
2022-06-03 23:39:09 +0200

    Reporting initial testing

--- verylinkedin/reporting/src/test/java/com/verylinkedin/reporting/ReportingApplicationTests.java
+++ verylinkedin/reporting/src/test/java/com/verylinkedin/reporting/ReportingApplicationTests.java
@@ -1,13 +1,28 @@
 package com.verylinkedin.reporting;
 
+import com.verylinkedin.reporting.commands.GetReport;
+import com.verylinkedin.reporting.requests.GetReportRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import com.verylinkedin.reporting.*;
 
-@SpringBootTest
+@SpringBootTest(classes = ReportingApplication.class)
 class ReportingApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    public void GetReport() {
+        Command cmd = applicationContext.getBean(GetReport.class);
+        GetReportRequest r = new GetReportRequest(1l);
+//        assertEquals(1, cmd.execute(r));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
