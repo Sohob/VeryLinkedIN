9d3893d9ca42bd237e64ee9a5d4a39c791eaab8e
Mohamed Sami <ma-771@hotmail.com>
2022-06-08 07:59:49 +0200

    Create Post test

--- verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/commands/CreatePost/CreatePostTest.java
+++ verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/commands/CreatePost/CreatePostTest.java
@@ -1,5 +1,7 @@
 package com.verylinkedin.mypost.commands.CreatePost;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.verylinkedin.mypost.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,27 +9,30 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 class CreatePostTest {
-
-    @Mock
-    private CreatePostRequest mockRequest;
+    private String userID = "userId";
+    private String content = "Hello World";
     @Mock
     private PostRepository mockPostRepository;
-
     private CreatePost createPostUnderTest;
 
     @BeforeEach
     void setUp() {
-        createPostUnderTest = new CreatePost(mockRequest, mockPostRepository);
+        createPostUnderTest = new CreatePost(new CreatePostRequest(userID, content), mockPostRepository);
     }
 
     @Test
     void testExecute() {
         // Setup
         // Run the test
-        final Object result = createPostUnderTest.execute();
-
+        final String result = (String) createPostUnderTest.execute();
+        JsonObject jsonObject = new JsonParser().parse(result).getAsJsonObject();
         // Verify the results
+
+        assertEquals(content, jsonObject.get("content").getAsString());
+        assertEquals(userID, jsonObject.get("userId").getAsString());
     }
 }
--- verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/commands/DeletePost/DeletePostTest.java
+++ verylinkedin/mypost/src/test/java/com/verylinkedin/mypost/commands/DeletePost/DeletePostTest.java
@@ -1,33 +1,62 @@
 package com.verylinkedin.mypost.commands.DeletePost;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.verylinkedin.mypost.PostRepository;
+import com.verylinkedin.mypost.commands.CreatePost.CreatePost;
+import com.verylinkedin.mypost.commands.CreatePost.CreatePostRequest;
+import com.verylinkedin.mypost.models.Post;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@AutoConfigureDataMongo
 @ExtendWith(MockitoExtension.class)
 class DeletePostTest {
-
-    @Mock
-    private DeletePostRequest mockRequest;
+    private String postId = "123456";
+    private String userId = "123456";
+    private String content = "Hello World";
     @Mock
     private PostRepository mockPostRepository;
-
+    private ArrayList<Post> postsRepo = new ArrayList<Post>();
     private DeletePost deletePostUnderTest;
+    private CreatePost createPostUnderTest;
 
     @BeforeEach
     void setUp() {
-        deletePostUnderTest = new DeletePost(mockRequest, mockPostRepository);
+        createPostUnderTest = new CreatePost(new CreatePostRequest(userId, content), mockPostRepository);
+        Post post = Post.builder()
+                .userId(userId)
+                .content(content)
+                .build();
+        post.setId(postId);
+        postsRepo.add(post);
+        mockPostRepository.save(post);
+        deletePostUnderTest = new DeletePost(new DeletePostRequest(postId), mockPostRepository);
     }
 
     @Test
     void testExecute() {
         // Setup
         // Run the test
-        final Object result = deletePostUnderTest.execute();
+        String output = "false";
+        final String resultCreate = (String) createPostUnderTest.execute();
+        JsonObject jsonObject = new JsonParser().parse(resultCreate).getAsJsonObject();
+        System.out.println(resultCreate);
+        final String result = (String) deletePostUnderTest.execute();
+        JsonObject jsonObjectDelete = new JsonParser().parse(result).getAsJsonObject();
 
+//        System.out.println(mockPostRepository.findAll());
+//        System.out.println(result);
+        assertEquals(jsonObjectDelete.get("success").getAsString(), "true");
         // Verify the results
     }
 }
